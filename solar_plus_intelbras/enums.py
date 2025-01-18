e64b5768a3f3df2157ac1abcdb6e4961d4afa862
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2025-01-18 02:12:55 -0300

    feat: add method _request

--- solar_plus_intelbras/enums.py
+++ solar_plus_intelbras/enums.py
@@ -1,6 +1,19 @@
 from enum import Enum
 
 
+class MethodEnum(Enum):
+    """A enum with the methods available in the API.
+
+    Args:
+        Enum (_type_): A class to represent a enumeration.
+    """
+
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    DELETE = "DELETE"
+
+
 class EndpointEnum(Enum):
     """A enum with the endpoints available in the API.
 
--- tests/test_solar_plus_intelbras.py
+++ tests/test_solar_plus_intelbras.py
@@ -1,3 +1,4 @@
+from solar_plus_intelbras.enums import EndpointEnum, MethodEnum
 from solar_plus_intelbras.solar_plus_intelbras import SolarPlusIntelbras
 
 
@@ -17,6 +18,14 @@ class TestSolarPlusIntelbras:
     ) -> None:
         assert solar_plus_intelbras.plus == "test"
 
+    def test_solar_plus_intelbras_base_api_url(
+        self, solar_plus_intelbras: SolarPlusIntelbras
+    ) -> None:
+        assert (
+            solar_plus_intelbras.base_api_url
+            == "https://ens-server.intelbras.com.br/api/"
+        )
+
     def test_solar_plus_intelbras_str(
         self, solar_plus_intelbras: SolarPlusIntelbras
     ) -> None:
@@ -26,3 +35,21 @@ class TestSolarPlusIntelbras:
         self, solar_plus_intelbras: SolarPlusIntelbras
     ) -> None:
         assert repr(solar_plus_intelbras) == "<SolarPlusIntelbras test@email.com>"
+
+    def test_solar_plus_intelbras_request(
+        self,
+        requests_mock,
+        solar_plus_intelbras: SolarPlusIntelbras,
+        login_response: dict,
+    ) -> None:
+        requests_mock.post(
+            "https://ens-server.intelbras.com.br/api/login",
+            json=login_response,
+            status_code=200,
+        )
+        assert (
+            solar_plus_intelbras._request(
+                method=MethodEnum.POST, endpoint=EndpointEnum.LOGIN
+            )
+            == login_response
+        )
