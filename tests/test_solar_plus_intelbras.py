7257f3df1882cc144e64b749c917d5e913c116c4
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2025-01-19 00:04:59 -0300

    feat: implements login method

--- tests/test_solar_plus_intelbras.py
+++ tests/test_solar_plus_intelbras.py
@@ -1,4 +1,5 @@
-from solar_plus_intelbras.enums import EndpointEnum, MethodEnum
+import pytest
+
 from solar_plus_intelbras.solar_plus_intelbras import SolarPlusIntelbras
 
 
@@ -36,7 +37,7 @@ class TestSolarPlusIntelbras:
     ) -> None:
         assert repr(solar_plus_intelbras) == "<SolarPlusIntelbras test@email.com>"
 
-    def test_solar_plus_intelbras_request(
+    def test_solar_plus_intelbras_token(
         self,
         requests_mock,
         solar_plus_intelbras: SolarPlusIntelbras,
@@ -48,8 +49,34 @@ class TestSolarPlusIntelbras:
             status_code=200,
         )
         assert (
-            solar_plus_intelbras._request(
-                method=MethodEnum.POST, endpoint=EndpointEnum.LOGIN
-            )
-            == login_response
+            solar_plus_intelbras.token
+            == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InBpY3R1cmUiOiIiLCJpZCI6Njg1MSwiZW1haWwiOiJjb250YXRvLmh1ZHNvbmJyZW5kb25AZ21haWwuY29tIiwibmFtZSI6Ikh1ZHNvbiBCcmVuZG9uIiwiY3BmIjoiIiwicGhvbmUiOiIiLCJjcmVhdGVkQXQiOiIyMDI0LTEyLTE2VDE0OjQ5OjU2LjYxMDY5OC0wMzowMCIsImFjdGl2YXRlZEF0Ijp7IlRpbWUiOiIyMDI0LTEyLTE2VDE0OjQ5OjU2LjYwODEwNS0wMzowMCIsIlZhbGlkIjp0cnVlfSwibGFzdExvZ2luIjoiMjAyNS0wMS0xNyAyMzoyNToxNC4zOTAwMjg0MzYgLTAzMDAgLTAzIG09KzUyMzkyLjQ4ODc5MjE1MSIsInByZWZlcmVuY2VzIjp7ImN1cnJlbmN5IjoiQlJMIiwidGltZXpvbmUiOiItMyIsInRlbXBlcmF0dXJlVW5pdCI6ImMiLCJzZW5kV2VhdGhlck5vdGlmaWNhdGlvbiI6dHJ1ZSwic2VuZEVuZXJneVRvZGF5Tm90aWZpY2F0aW9uIjp0cnVlLCJzZW5kQWxlcnROb3RpZmljYXRpb24iOnRydWV9LCJyZWdpc3RyYXRpb25Ub2tlbiI6ImQxbWNVRzNVSjA5UHN3d0JDLXVFUDk6QVBBOTFiRzJ4Z3dXUDIzVFhab0tiejVIamZpQW9EbnIxeGEtT1haVTZDMXVEQWIyczZPU3NkRHdMQ3VYTWxkdnBYM0ZJcF9jQloxRU4zU2xsZm5nWmVCYU1aNUNsLWhvcGNLajNhWnRWTTE0MEsyT1U4Tno3eHMiLCJhY3RpdmF0aW9uQ29kZSI6ImU2YjE2OGRkZjc0M2QwOWE4OGU2NmYwZWZhMDZiMDE1In0sImV4cCI6MTczNzE3NTcxNiwiaWF0IjoxNzM3MTc1NDE2LCJjb2RlIjoiIiwicm9sZSI6ImNsaWVudCJ9.FXOp76WgRRemmiOr2z4ML6QVEG1fbDYk1LfYK_hm3vs"
         )
+
+    def test_solar_plus_intelbras_login(
+        self,
+        requests_mock,
+        solar_plus_intelbras: SolarPlusIntelbras,
+        login_response: dict,
+    ) -> None:
+        requests_mock.post(
+            "https://ens-server.intelbras.com.br/api/login",
+            json=login_response,
+            status_code=200,
+        )
+        assert solar_plus_intelbras._login() == login_response
+
+    def test_solar_plus_intelbras_token_error(
+        self,
+        requests_mock,
+        login_response: dict,
+        solar_plus_intelbras: SolarPlusIntelbras,
+    ) -> None:
+        login_response.pop("accessToken")
+        requests_mock.post(
+            "https://ens-server.intelbras.com.br/api/login",
+            json=login_response,
+            status_code=200,
+        )
+        with pytest.raises(KeyError):
+            solar_plus_intelbras.token
