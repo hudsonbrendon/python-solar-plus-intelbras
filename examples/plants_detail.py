f20c40f7a8c23d5b65965a75402c01b207f4afad
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2025-02-05 00:24:49 -0300

    feat: add examples

---
+++ examples/plants_detail.py
@@ -0,0 +1,12 @@
+from decouple import config
+
+from solar_plus_intelbras import SolarPlusIntelbras
+
+solar_plus_intelbras = SolarPlusIntelbras(
+    email=config("SOLAR_PLUS_EMAIL"),
+    plus=config("SOLAR_PLUS_PLUS"),
+)
+
+
+if __name__ == "__main__":
+    print(solar_plus_intelbras.plants_detail(plant_id=config("SOLAR_PLUS_PLANT_ID")))
---
+++ examples/records_year.py
@@ -0,0 +1,16 @@
+from decouple import config
+
+from solar_plus_intelbras import SolarPlusIntelbras
+
+solar_plus_intelbras = SolarPlusIntelbras(
+    email=config("SOLAR_PLUS_EMAIL"),
+    plus=config("SOLAR_PLUS_PLUS"),
+)
+
+
+if __name__ == "__main__":
+    print(
+        solar_plus_intelbras.records_year(
+            year=2025, plant_id=config("SOLAR_PLUS_PLANT_ID")
+        )
+    )
